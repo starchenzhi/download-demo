6ab18033b62af2cefe93a0b888ed24094a3974ed
Zhi Chen <billchengreat@gmail.com>
2021-08-06 20:44:48 -0400

    open a new window to show pdf

--- src/App.js
+++ src/App.js
@@ -9,36 +9,37 @@ function App() {
       let getWHeadersConfig = { responseType: "arraybuffer" }
       let response = await axios.get(apiURL, getWHeadersConfig)
       if (response && response.data) {
-        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-          const urlIE = new Blob([response.data], { type: "application/pdf" })
-          window.navigator.msSaveOrOpenBlob(urlIE, "Consent.pdf")
-        } else if (/iP(hone|od|ad)/.test(navigator.platform)) {
-          const blob = new Blob([response.data], { type: "application/pdf" })
-          const url = window.URL.createObjectURL(blob)
-          window.open(url)
-        } else {
-          const a = document.createElement("a");
-          const contentDisposition = "content-disposition"
-          document.body.appendChild(a)
-          a.style = "display: none"
-          const blob = new Blob([response.data], { type: "application/pdf" })
-          const url = window.URL.createObjectURL(blob)
-          a.href = url
-          if (response.headers && response.headers[contentDisposition]) {
-            let downloadName = response.headers[contentDisposition]
-            const searchString = "filename="
-            downloadName = downloadName.substring(downloadName.length, downloadName.indexOf(searchString) + searchString.length)
-            a.download = downloadName
-          } else {
-            a.download = "Consent.pdf"
-          }
+        window.open(apiURL)
+        // if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        //   const urlIE = new Blob([response.data], { type: "application/pdf" })
+        //   window.navigator.msSaveOrOpenBlob(urlIE, "Consent.pdf")
+        // } else if (/iP(hone|od|ad)/.test(navigator.platform)) {
+        //   const blob = new Blob([response.data], { type: "application/pdf" })
+        //   const url = window.URL.createObjectURL(blob)
+        //   window.open(url)
+        // } else {
+        //   const a = document.createElement("a");
+        //   const contentDisposition = "content-disposition"
+        //   document.body.appendChild(a)
+        //   a.style = "display: none"
+        //   const blob = new Blob([response.data], { type: "application/pdf" })
+        //   const url = window.URL.createObjectURL(blob)
+        //   a.href = url
+        //   if (response.headers && response.headers[contentDisposition]) {
+        //     let downloadName = response.headers[contentDisposition]
+        //     const searchString = "filename="
+        //     downloadName = downloadName.substring(downloadName.length, downloadName.indexOf(searchString) + searchString.length)
+        //     a.download = downloadName
+        //   } else {
+        //     a.download = "Consent.pdf"
+        //   }
 
-          a.click()
-          window.URL.revokeObjectURL(url)
-        }
+        //   a.click()
+        //   window.URL.revokeObjectURL(url)
+        // }
       }
 
-      console.log("##### the code to reproduce crash.")
+      console.log("##### open a new window to show pdf.")
 
     } catch (error) {
       window.open(apiURL)
